e40dba443e82b3e390e4503e2d387133eb261bfc
JXPM <bilejohan04@gmail.com>
2025-12-05 00:10:32 +0100

    footer

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,12 +1,13 @@
-import { Link } from "react-router-dom"
+import Footer from "../Footer"
 
-function Home() {
-    return (
-        <>
-            <Link to="/fake-os">Fake OS</Link>
-            <Link to="/complicated-form">Complicated form</Link>
-        </>
-    )
+function App() {
+  return (
+    <div className="app">
+      {/* ton desktop ici */}
+
+      <Footer />
+    </div>
+  );
 }
 
-export default Home
+export default App;
