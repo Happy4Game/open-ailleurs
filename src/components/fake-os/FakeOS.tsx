dd4b6abe1410603de15ff2d53619c2a5f6ac3df9
Happy4Game <dufeutrel.thibaut@gmail.com>
2025-12-04 23:54:42 +0100

    feat: Ajout des logos d'applications

--- src/components/fake-os/FakeOS.tsx
+++ src/components/fake-os/FakeOS.tsx
@@ -1,11 +1,28 @@
 import './FakeOs.css'
 import linux from '../../assets/linux.png'
+import libreoffice from '../../assets/appLogos/libreoffice.png'
+import scratch from '../../assets/appLogos/scratch.png'
+import qrprint from '../../assets/appLogos/QRPrint.png'
 
 function FakeOS() {
     return (
         <>
             <div className='fakeos-body'>
                 <img className='linux-img' src={linux} alt="Image de fond" />
+                <div className='list-app-row'>
+                    <div>
+                        <img src={libreoffice} alt="Libreoffice" />
+                        <p>Libreoffice</p>
+                    </div>
+                    <div>
+                        <img src={scratch} alt="Scratch" />
+                        <p>Scratch</p>
+                    </div>
+                    <div>
+                        <img src={qrprint} alt="QRPrint" />
+                        <p>QRPrint</p>
+                    </div>
+                </div>
             </div>
         </>
     )
