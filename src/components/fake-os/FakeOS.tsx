0e868680e95d6189bbf70368828f05e36416f3af
Happy4Game <dufeutrel.thibaut@gmail.com>
2025-12-05 03:29:44 +0100

    feat: multiple app added

--- src/components/fake-os/FakeOS.tsx
+++ src/components/fake-os/FakeOS.tsx
@@ -3,6 +3,11 @@ import linux from '../../assets/linux.png'
 import libreoffice from '../../assets/appLogos/libreoffice.png'
 import scratch from '../../assets/appLogos/scratch.png'
 import qrprint from '../../assets/appLogos/QRPrint.png'
+import blender from '../../assets/appLogos/blender.png'
+import vlc from '../../assets/appLogos/vlc.png'
+import git from '../../assets/appLogos/git.png'
+import geogebra from '../../assets/appLogos/geogebra.png'
+import stellarium from '../../assets/appLogos/stellarium.png'
 
 function FakeOS() {
     return (
@@ -21,6 +26,32 @@ function FakeOS() {
                     <div>
                         <img src={qrprint} alt="QRPrint" />
                         <p>QRPrint</p>
+                    </div>
+                    
+                </div>
+                <div className='list-app-row'>
+                    <div>
+                        <img src={blender} alt="Blender" />
+                        <p>Blender</p>
+                    </div>
+                    <div>
+                        <img src={vlc} alt="VLC" />
+                        <p>VLC</p>
+                    </div>
+                </div>
+                <div className='list-app-row'></div>
+                <div className='list-app-row'>
+                    <div>
+                        <img src={git} alt="git" />
+                        <p>Git</p>
+                    </div>
+                    <div>
+                        <img src={geogebra} alt="GeoGebra" />
+                        <p>GeoGebra</p>
+                    </div>
+                    <div>
+                        <img src={stellarium} alt="Stellarium" />
+                        <p>Stellarium</p>
                     </div>
                 </div>
             </div>
