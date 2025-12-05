332eb6aad5a1169a655c7c0aee0137df306a7b73
Happy4Game <dufeutrel.thibaut@gmail.com>
2025-12-05 01:42:03 +0100

    feat: implement header and footer

--- src/components/fake-os/FakeOS.tsx
+++ src/components/fake-os/FakeOS.tsx
@@ -3,10 +3,13 @@ import linux from '../../assets/linux.png'
 import libreoffice from '../../assets/appLogos/libreoffice.png'
 import scratch from '../../assets/appLogos/scratch.png'
 import qrprint from '../../assets/appLogos/QRPrint.png'
+import Footer from '../Footer'
+import Header from '../Header'
 
 function FakeOS() {
     return (
         <>
+        <Header></Header>
             <div className='fakeos-body'>
                 <img className='linux-img' src={linux} alt="Image de fond" />
                 <div className='list-app-row'>
@@ -24,6 +27,7 @@ function FakeOS() {
                     </div>
                 </div>
             </div>
+            <Footer></Footer>
         </>
     )
 }
